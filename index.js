45b3eaea38b7feea3a82cd5c7b9b9939e338d97d
Linou <lina_abuhijleh@hotmail.fr>
2022-12-03 09:08:18 +0100

    test

---
+++ index.js
@@ -0,0 +1,11 @@
+
+ReactDOM.render(
+    <MainContent/>, 
+    document.getElementById("root"))
+
+    function MainContent(){
+        return(
+            <h1>I am learning React!</h1>
+        )
+    }
+
